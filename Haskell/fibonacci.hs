274044822671ff43cf0f8c97a0a8002067cb5fce
shyling <450044101@qq.com>
2016-04-27 16:57:35 +0800

    Update fibonacci.hs

--- Haskell/fibonacci.hs
+++ Haskell/fibonacci.hs
@@ -1,15 +1,15 @@
 import System.Environment
-
-fib :: Int -> Integer            
-fib = (map fib' [0..] !!)        
-  where fib' 0 = 0               
-        fib' 1 = 1               
+        
+fib :: Int -> Integer    
+fib = (map fib' [0..] !!)    
+  where fib' 0 = 0    
+        fib' 1 = 1    
         fib' i = fib(i-2)+fib(i-1)
-
+        
 main :: IO()  
 main = do
   args <- getArgs
-  putStrLn $ case length args of
-    0 -> "need param"
-    _ -> show $ fib $ n
-      where n = read(args !! 0)
+  print $ case null args of
+    True -> 0                                                                           
+    _ -> fib n
+      where n = read(head args)
