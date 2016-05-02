f6152809d76cd3b93b45966b68fbd52f9b96e3b4
shyling <450044101@qq.com>
2016-05-02 15:05:49 +0800

    Update quick-sort.js
    去掉不需要的n次递归

--- NodeJS/quick-sort.js
+++ NodeJS/quick-sort.js
@@ -1,6 +1,10 @@
 function qsort(...arr) {
-  let key = arr.shift();
-  return key === undefined ? [] : qsort(...arr.filter(_ => _ <= key)).concat([key]).concat(qsort(...arr.filter(_ => _ > key)));
+  if (arr.length <= 1) {
+    return arr;
+  } else {
+    let key = arr.shift();
+    return qsort(...arr.filter(_ => _ <= key)).concat([key]).concat(qsort(...arr.filter(_ => _ > key)));
+  }
 }
 
 console.log(qsort(1, 3, 2, 4, 6, 10, 8, 9, 11, 99, 100, 7));
