4a603a86b0c14ceaf589c0772d2db1b77dfdea1b
shyling <450044101@qq.com>
2016-05-02 13:10:47 +0800

    Create merge-sort.js

---
+++ NodeJS/merge-sort.js
@@ -0,0 +1,22 @@
+function merge(left = [], right = []) {
+	let [l, r, result] = [0, 0, []];
+	while (l < left.length && r < right.length) {
+		if (left[l] < right[r]) {
+			result.push(left[l++]);
+		} else {
+			result.push(right[r++]);
+		}
+	}
+	return result.concat(left.slice(l)).concat(right.slice(r));
+}
+
+function sort(...arr) {
+	if (arr.length <= 1) {
+		return arr;
+	} else {
+		let len = arr.length / 2;
+		return merge(sort(...arr.slice(0, len)), sort(...arr.slice(len)));
+	}
+}
+
+console.log(sort(10,9,8,7,6,5,4,3,2,1));
