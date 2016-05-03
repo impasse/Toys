879d0f05c0f72a19739029f1c3d63f79439ae9a5
shyling <450044101@qq.com>
2016-05-03 22:11:20 +0800

    Create binarytree-walk.js

---
+++ NodeJS/binarytree-walk.js
@@ -0,0 +1,52 @@
+class Node{
+	constructor(value,left,right) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+function DLR(n) {
+	if (n === undefined) {
+		return;
+	}
+	console.log(n.value);
+	DLR(n.left);
+	DLR(n.right);
+}
+
+function LDR(n) {
+	if (n === undefined) {
+		return;
+	}
+	LDR(n.left);
+	console.log(n.value);
+	LDR(n.right);
+}
+function LRD(n) {
+	if (n === undefined) {
+		return;
+	}
+	LRD(n.left);
+	LRD(n.right);
+	console.log(n.value);
+}
+
+/*
+		A
+      /   \
+	 B     C 
+    / \   /
+   D   E F
+*/
+
+let n = new Node('A',
+	new Node('B', new Node('D'), new Node('E')),
+	new Node('C', new Node('F'))
+);
+
+DLR(n);
+console.log('----');
+LDR(n);
+console.log('----');
+LRD(n);
