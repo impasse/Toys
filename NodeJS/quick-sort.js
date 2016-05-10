82533d3507c600a309fab02c9cf8a0188a56bed5
shyling <450044101@qq.com>
2016-05-10 13:30:00 +0800

    Update quick-sort.js

--- NodeJS/quick-sort.js
+++ NodeJS/quick-sort.js
@@ -1,23 +1,5 @@
-Array.prototype.partition = function (guard = true) {
-  let a = [], b = [];
-  for (let i of this) {
-    if (guard(i)) {
-      a.push(i);
-    } else {
-      b.push(i);
-    }
-  }
-  return [a, b];
-}
-
-function qsort(...arr) {
-  if (arr.length <= 1) {
-    return arr;
-  } else {
-    let key = arr.shift();
-    let [lower, higher] = arr.partition(_ => _ <= key);
-    return qsort(...lower).concat([key], qsort(...higher));
-  }
+function qsort(head, ...tail) {
+  return head == undefined ? [] : qsort(...tail.filter(_ => _ <= head)).concat(head,qsort(...tail.filter(_ => _ > head)));
 }
 
 console.log(qsort(1, 3, 2, 4, 6, 10, 8, 9, 11, 99, 100, 7));
