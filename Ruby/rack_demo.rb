cd489df9295e4ce4b8677737a7edc8a86a5db409
shyling <450044101@qq.com>
2016-06-06 22:26:16 +0800

    Update rack_demo.rb

--- Ruby/rack_demo.rb
+++ Ruby/rack_demo.rb
@@ -1,7 +1,9 @@
+#!/usr/bin/ruby
+# -*- encoding: utf-8 -*-
 require 'rack'
 
 app = proc do |env|
-    ['200',{'Content-Type'=>'text/plain'},[env.map{|k,v| "#{k}:#{v}"}.join("\r\n")]]
+    ['200',{'Content-Type'=>'text/plain; charset=utf-8'},[env.map{|k,v| "#{k}:#{v}"}.join("\r\n")]]
 end
 
 Rack::Handler::WEBrick.run app
