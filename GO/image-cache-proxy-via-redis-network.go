ab57afc0179c3be02d6a122c8a400a25106e161b
shyling <450044101@qq.com>
2016-06-27 16:38:27 +0800

    增加自动转为webp格式

--- GO/image-cache-proxy-via-redis-network.go
+++ GO/image-cache-proxy-via-redis-network.go
@@ -8,11 +8,13 @@ import (
 	"strings"
 	"time"
 
+	"gopkg.in/h2non/bimg.v1"
 	"gopkg.in/redis.v3"
 )
 
 const (
-	expries = 12
+	expries       = 12
+	convertToWebP = true
 )
 
 var client = redis.NewClient(&redis.Options{
@@ -42,8 +44,16 @@ func getImageFromRedisOrNetwork(url string) ([]byte, map[string]string) {
 func putImageToRedis(url string, bs []byte, head http.Header) {
 	if len(bs) != 0 {
 		client.Pipelined(func(pipe *redis.Pipeline) error {
+			mime := head.Get("Content-Type")
+			if convertToWebP {
+				newImage, err := bimg.NewImage(bs).Convert(bimg.WEBP)
+				if err == nil {
+					bs = newImage
+					mime = "image/webp"
+				}
+			}
 			pipe.HSet(url, "data", string(bs))
-			pipe.HSet(url, "type", head.Get("Content-Type"))
+			pipe.HSet(url, "type", mime)
 			pipe.Expire(url, expries*time.Hour)
 			return nil
 		})
@@ -52,6 +62,7 @@ func putImageToRedis(url string, bs []byte, head http.Header) {
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	fullURL := "http:/" + r.URL.RequestURI()
+	isAcceptWebP := strings.Index(r.Header.Get("Accept"), "image/webp") != -1
 	defer func() {
 		logger.Printf("%v - %v", time.Now().Format(time.UnixDate), fullURL)
 		if err := recover(); err != nil {
@@ -64,7 +75,13 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		for k, v := range head {
 			w.Header().Set(k, v)
 		}
-		w.Write(bs)
+		if isAcceptWebP {
+			w.Write(bs)
+		} else {
+			w.Header().Set("Content-Type", "image/jpeg")
+			bs, _ = bimg.NewImage(bs).Convert(bimg.JPEG)
+			w.Write(bs)
+		}
 	}
 }
 
