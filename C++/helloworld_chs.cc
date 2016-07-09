ac54312f2ddfdcc86d60dd671e51caa2e1707722
shyling <450044101@qq.com>
2016-07-09 12:14:55 +0800

    Create helloworld_chs.cc

---
+++ C++/helloworld_chs.cc
@@ -0,0 +1,18 @@
+#include<cstdio>
+
+#define 整型 int
+#define 主函数 main
+#define 左大括号 {
+#define 右大括号 }
+#define 左小括号 (
+#define 右小括号 )
+#define 返回 return
+#define 控制台输出 printf
+#define 分号 ;
+#define 零 0;
+#define 字符串(s) #s
+
+整型 主函数 左小括号 右小括号 左大括号
+控制台输出 左小括号 字符串(你好世界)  右小括号 分号
+返回 零 分号
+右大括号
